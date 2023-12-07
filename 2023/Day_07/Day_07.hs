06df31b71962f78f088457b1cb10b4e8f860410d
Sheinxy <sheinxy@seascow.win>
2023-12-07 11:25:28 +0100

    Day_07(2023): solved

---
+++ 2023/Day_07/Day_07.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Function
+import Data.List
+import Data.Map (Map, (!), fromList)
+import Data.Ord
+import Data.Text (replace, unpack, pack)
+import Data.Tuple.Extra (both)
+import System.Environment
+
+data Hand = Hand { cards :: String, bid :: Int, hType :: Type, order :: String} deriving (Eq, Show)
+data Type = High | One | Two | Three | Full | Four | Five deriving (Eq, Ord, Show)
+
+instance Ord Hand where
+    (Hand c1 _ t1 o1) `compare` (Hand c2 _ t2 o2) 
+        | o1 /= o2  = error "Comparing different games"
+        | t1 /= t2  = t1 `compare` t2
+        | otherwise = uncurry (compare `on` (cardStrength !)) .
+                      head . dropWhile (uncurry (==)) $ zip c1 c2
+        where cardStrength = fromList $ zip o1 [1 .. ]
+
+type Input = [Hand]
+type Output = Int
+
+getType :: String -> Type
+getType = go . sort . map length . group . sort
+    where go [5]          = Five
+          go [1, 4]       = Four
+          go [2, 3]       = Full
+          go [1, 1, 3]    = Three
+          go [1, 2, 2]    = Two
+          go [1, 1, 1, 2] = One
+          go _            = High
+
+getJokeType :: String -> Type
+getJokeType s = maximum . map getType . map replaceJ $ "23456789TQKA"
+    where replaceJ c = unpack . replace "J" (pack [c]) . pack $ s
+
+parseInput :: String -> Input
+parseInput =  map ((\[c, b] -> Hand c (read b) (getType c) "23456789TJQKA") . words) . lines
+
+partOne :: Input -> Output
+partOne = sum . map (\(n, h) -> n * bid h) . zip [1 .. ] . sort
+
+partTwo :: Input -> Output
+partTwo = partOne . map convertHand
+    where convertHand h = Hand (cards h) (bid h) (getJokeType $ cards h) "J23456789TQKA"
+
+compute :: Input -> String -> IO ()
+compute input "parse" = print input
+compute input "one"   = print . partOne $ input
+compute input "two"   = print . partTwo $ input
+compute input _       = error "Unknown part"
+
+main = do
+    args  <- getArgs
+    input <- parseInput <$> (readFile $ last args)
+    mapM (compute input)  $ init args 
--- 2023/README.md
+++ 2023/README.md
@@ -11,7 +11,7 @@ Watch me code in Haskell for 25 days straight
 |  [Day 04](./Day_04)	|    ✅    	|    ✅    	|  [Day 13](./Day_13)	|         	|        	|  [Day 22](./Day_22)	|        	|        	|
 |  [Day 05](./Day_05)	|    ✅    	|    ✅    	|  [Day 14](./Day_14)	|         	|        	|  [Day 23](./Day_23)	|        	|        	|
 |  [Day 06](./Day_06)	|    ✅   	|    ✅    	|  [Day 15](./Day_15)	|         	|        	|  [Day 24](./Day_24)	|        	|        	|
-|  [Day 07](./Day_07)	|         	|         	|  [Day 16](./Day_16)	|         	|        	|  [Day 25](./Day_25)	|        	|        	|
+|  [Day 07](./Day_07)	|    ✅    	|    ✅    	|  [Day 16](./Day_16)	|         	|        	|  [Day 25](./Day_25)	|        	|        	|
 |  [Day 08](./Day_08)	|         	|         	|  [Day 17](./Day_17)	|         	|        	|     	|        	|        	|
 |  [Day 09](./Day_09)	|         	|         	|  [Day 18](./Day_18)	|         	|        	|     	|        	|        	|
 
@@ -46,3 +46,6 @@ Challenging enough to make me take out a pen and paper, simple enough to make me
 I am now convinced that odd days are hard and even days are easy.
 
 This is just simple maths!
+
+### [Day 07](./Day_07)
+Not too hard, so I decided to have fun with Instances!
