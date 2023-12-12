51b075c8f720ac7fa5b3201cea6a1b97ce94cdf4
Sheinxy <raphael.montes@epita.fr>
2023-12-12 13:54:28 +0100

    Day_12(2023): better index

--- 2023/Day_12/Day_12.hs
+++ 2023/Day_12/Day_12.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.List (intercalate)
 import Data.Maybe (isJust, fromJust)
-import Data.Map hiding (map)
+import Data.Map hiding (map, filter)
 import qualified Data.Map as M (lookup)
 import Data.Tuple.Extra ((***))
 import System.Environment
@@ -15,7 +15,7 @@ parseInput = map (go . words) . lines
     where go [s, ns] = (s, read $ "[" ++ ns ++ "]")
 
 -- Memoization types
-type Key = (String, Int, [Int]) -- (Tiles, Current block length, Groups left)
+type Key = (Int, [Int], String) -- (Current block length, groups, tiles)
 type Memo = Map Key Int         -- State -> Number of arrangement given by that state
 
 -- Update the cache and return both the cache and the value
@@ -26,27 +26,27 @@ updateMemo m k v = (insert k v m, v)
 computeWithMemo :: Memo -> Key -> (Memo, Int)
 computeWithMemo m k | isJust r = (m, fromJust r) where r = M.lookup k m                         -- Cached result
 
-computeWithMemo m k@(_, n, c:_) | n > c = updateMemo m k 0                                      -- Invalid arrangement: current block is too big
+computeWithMemo m k@(n, c:_, _)  | n > c = updateMemo m k 0                                      -- Invalid arrangement: current block is too big
 
-computeWithMemo m k@(xs      , 0, [ ]) | all (`elem` "?.") xs           = updateMemo m k 1      -- Valid arrangement: end of groups
-computeWithMemo m k@(xs      , _, [ ]) | '#'  `elem`       xs           = updateMemo m k 0      -- Invalid arrangement: more blocks but no more groups
-computeWithMemo m k@(xs      , n, [r]) | all (`elem` "?.") xs && r == n = updateMemo m k 1      -- Valid arrangement: current block ended with the right length for the last group
-computeWithMemo m k@(""      , _,  _ ) = updateMemo m k 0                                       -- Invalid arrangement: end of strings with leftover groups
+computeWithMemo m k@(0, [ ], xs) | all (`elem` "?.") xs           = updateMemo m k 1      -- Valid arrangement: end of groups
+computeWithMemo m k@(_, [ ], xs) | '#'  `elem`       xs           = updateMemo m k 0      -- Invalid arrangement: more blocks but no more groups
+computeWithMemo m k@(n, [r], xs) | all (`elem` "?.") xs && r == n = updateMemo m k 1      -- Valid arrangement: current block ended with the right length for the last group
+computeWithMemo m k@(n, _,   "") = updateMemo m k 0                                       -- Invalid arrangement: end of strings with leftover groups
 
-computeWithMemo m k@('.' : xs, 0, groups) = updateMemo m' k v                                   -- Case '.' while not on block: keep going
-                                          where (m', v) = computeWithMemo m (xs, 0, groups)
-computeWithMemo m k@('.' : xs, n, c:rest) | n == c      = updateMemo m' k v                     -- Case '.' while on block: block has the right length
+computeWithMemo m k@(0, groups, '.' : xs) = updateMemo m' k v                                   -- Case '.' while not on block: keep going
+                                          where (m', v) = computeWithMemo m (0, groups, xs)
+computeWithMemo m k@(n, c:rest, '.' : xs) | n == c      = updateMemo m' k v                     -- Case '.' while on block: block has the right length
                                           | otherwise   = updateMemo m k 0                      -- Case '.' while on block: block has not the right length
-                                          where (m', v) = computeWithMemo m (xs, 0, rest)
+                                          where (m', v) = computeWithMemo m (0, rest, xs)
 
-computeWithMemo m k@('#' : xs, n, groups) = updateMemo m' k v                                   -- Case '#': keep going and count this tile in the block length
-                                          where (m', v) = computeWithMemo m (xs, n + 1, groups)
+computeWithMemo m k@(n, groups, '#' : xs) = updateMemo m' k v                                   -- Case '#': keep going and count this tile in the block length
+                                          where (m', v) = computeWithMemo m (n + 1, groups, xs)
 
-computeWithMemo m k@('?' : xs, n, groups) = updateMemo m'' k (v + v')                           -- Case '?': Check what happens if '.' and if '#'. Add the number of arrangement for each possibility
-                                          where (m' , v ) = computeWithMemo m  ('.' : xs, n, groups)
-                                                (m'', v') = computeWithMemo m' ('#' : xs, n, groups)
+computeWithMemo m k@(n, groups, '?' : xs) = updateMemo m'' k (v + v')                           -- Case '?': Check what happens if '.' and if '#'. Add the number of arrangement for each possibility
+                                          where (m' , v ) = computeWithMemo m  (n, groups, '.' : xs) 
+                                                (m'', v') = computeWithMemo m' (n, groups, '#' : xs)
 partOne :: Input -> Output
-partOne = sum . map (\(s, g) -> snd . computeWithMemo empty $ (s, 0, g))
+partOne = sum . map (\(s, g) -> snd . computeWithMemo empty $ (0, g, s))
 
 partTwo :: Input -> Output
 partTwo = partOne . map (intercalate "?" . replicate 5 *** concat . replicate 5)
