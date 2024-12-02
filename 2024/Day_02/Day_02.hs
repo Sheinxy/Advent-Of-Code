425f8765c584b0f632c0fc8318d449e7179b5040
Sheinxy <sheinxy@seascow.win>
2024-12-02 12:36:33 +0100

    feat(2024/Day_02): Day_02

---
+++ 2024/Day_02/Day_02.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import System.Environment
+import Data.List
+
+type Input = [[Int]]
+type Output = Int
+
+parseInput :: String -> Input
+parseInput = map (map read . words) . lines
+
+partOne :: Input -> Output
+partOne = length . filter isSafe . map computeDiff
+    where computeDiff report = zipWith (-) report $ tail report
+          isSafe diff = all (\x -> 1 <= x && x <= 3) diff || all (\x -> -3 <= x && x <= -1) diff
+
+partTwo :: Input -> Output
+partTwo = length . filter id . map (any (isSafe . computeDiff) . computePossibilities)
+    where computePossibilities l = filter (\x -> abs (length l - length x) <= 1) $ subsequences l
+          computeDiff report = zipWith (-) report $ tail report
+          isSafe diff = all (\x -> 1 <= x && x <= 3) diff || all (\x -> -3 <= x && x <= -1) diff
+
+compute :: Input -> String -> IO ()
+compute input "parse" = print input
+compute input "one"   = print . partOne $ input
+compute input "two"   = print . partTwo $ input
+compute input _       = error "Unknown part"
+
+main = do
+    args  <- getArgs
+    input <- parseInput <$> readFile (last args)
+    mapM (compute input) $  init args
