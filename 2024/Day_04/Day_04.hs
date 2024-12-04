f59ea0f7066122be1d86530025d1126743a88e33
Sheinxy <sheinxy@seascow.win>
2024-12-04 14:56:04 +0100

    feat(2024/Day_04): Day_04

---
+++ 2024/Day_04/Day_04.hs
@@ -0,0 +1,49 @@
+module Main where
+
+import System.Environment
+import Data.List
+import Text.Regex.TDFA
+
+type Input = [String]
+type Output = Int
+
+parseInput :: String -> Input
+parseInput = lines
+
+-- Not the same as Data.List.Split's chunksOf
+-- This gives all the complete subsequences of length n
+-- That is like iterating over take (length l - x) $ drop x l
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf n = concatMap (filter ((== n) . length) . inits) . tails
+
+partOne :: Input -> Output
+partOne input = searchHorizontal input + searchVertical input + searchDiagonal input
+    where search :: String -> [String]
+          search l = concat (l =~ "XMAS" :: [[String]])
+          searchHorizontal l = length $ concatMap (search . reverse) l ++ concatMap search l
+          searchVertical     = searchHorizontal . transpose
+          searchDiagonal  l  = countInDiagonal l + countInDiagonal (reverse l)
+            where shift l         = [drop i x | (i, x) <- zip [0 .. ] l]
+                  getDiagonals    = map shift . chunksOf 4
+                  countInDiagonal = sum . map searchVertical . getDiagonals
+
+
+partTwo :: Input -> Output
+partTwo =  length . filter id . concatMap (zipWith3' isCross . map (chunksOf 3)) . chunksOf 3
+    where zipWith3' f [a, b, c] = zipWith3 f a b c
+          isCross ['M', _, 'S'] [_, 'A', _] ['M', _, 'S'] = True
+          isCross ['S', _, 'M'] [_, 'A', _] ['S', _, 'M'] = True
+          isCross ['M', _, 'M'] [_, 'A', _] ['S', _, 'S'] = True
+          isCross ['S', _, 'S'] [_, 'A', _] ['M', _, 'M'] = True
+          isCross _ _ _                                   = False
+
+compute :: Input -> String -> IO ()
+compute input "parse" = print input
+compute input "one"   = print . partOne $ input
+compute input "two"   = print . partTwo $ input
+compute input _       = error "Unknown part"
+
+main = do
+    args  <- getArgs
+    input <- parseInput <$> readFile (last args)
+    mapM (compute input) $  init args 
--- 2024/README.md
+++ 2024/README.md
@@ -8,7 +8,7 @@ Watch me code in Haskell for 25 days straight
 |  [Day 01](./Day_01)	|     ✅  	|     ✅   	|  [Day 10](./Day_10)	|        	|         	|  [Day 19](./Day_19)	|         	|         	|
 |  [Day 02](./Day_02)	|     ✅   	|     ✅   	|  [Day 10](./Day_11)	|          |         	|  [Day 20](./Day_20)	|         	|         	|
 |  [Day 03](./Day_03)	|     ✅   	|     ✅   	|  [Day 12](./Day_12)	|         	|          |  [Day 21](./Day_21)	|         	|         	|
-|  [Day 04](./Day_04)	|         	|         	|  [Day 13](./Day_13)	|          |         	|  [Day 22](./Day_22)	|         	|         	|
+|  [Day 04](./Day_04)	|     ✅   	|     ✅   	|  [Day 13](./Day_13)	|          |         	|  [Day 22](./Day_22)	|         	|         	|
 |  [Day 05](./Day_05)	|         	|         	|  [Day 14](./Day_14)	|          |          |  [Day 23](./Day_23)	|         	|        	|
 |  [Day 06](./Day_06)	|        	|         	|  [Day 15](./Day_15)	|          |         	|  [Day 24](./Day_24)	|        	|        	|
 |  [Day 07](./Day_07)	|         	|         	|  [Day 16](./Day_16)	|          |         	|  [Day 25](./Day_25)	|        	|        	|
@@ -30,3 +30,7 @@ Once again a very simple day, but a fun one!
 ### [Day 03](./Day_03)
 
 Regex goes brrrrrrrrrrr!
+
+### [Day 04](./Day_04)
+
+2D grid search goes noooooooooooooooooooo!
