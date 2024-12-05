8aeeb50bba7a5db4d829ef2228ff3b6c554e9d75
Sheinxy <sheinxy@seascow.win>
2024-12-05 07:10:42 +0100

    feat(2024/Day_05): Day_05

---
+++ 2024/Day_05/Day_05.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import System.Environment
+import Data.List
+
+type Input = ([(Int, Int)], [[Int]])
+type Output = Int
+
+parseInput :: String -> Input
+parseInput = getInput . break null . lines
+    where getInput (orders, _ : numbers) = (map readOrder orders, map readNumber numbers)
+          readOrder n = (read (fst $ span (/= '|') n), read (tail . snd $ span (/= '|') n))
+          readNumber n = read ("[" ++ n ++ "]") :: [Int]
+
+getCorrectOrder :: [(Int, Int)] -> Int -> Int -> Ordering
+getCorrectOrder orders a b
+            | (a, b) `elem` orders = LT
+            | (b, a) `elem` orders = GT
+            | otherwise            = EQ
+
+middle :: [a] -> a
+middle l = head $ drop (length l `div` 2) l
+
+partOne :: Input -> Output
+partOne (orders, input) = sum [middle a | (a, b) <- (zip ordered input), a == b]
+    where ordered = map (sortBy $ getCorrectOrder orders) input
+
+partTwo :: Input -> Output
+partTwo (orders, input) = sum [middle a | (a, b) <- (zip ordered input), a /= b]
+    where ordered = map (sortBy $ getCorrectOrder orders) input
+
+compute :: Input -> String -> IO ()
+compute input "parse" = print input
+compute input "one"   = print . partOne $ input
+compute input "two"   = print . partTwo $ input
+compute input _       = error "Unknown part"
+
+main = do
+    args  <- getArgs
+    input <- parseInput <$> readFile (last args)
+    mapM (compute input) $  init args 
