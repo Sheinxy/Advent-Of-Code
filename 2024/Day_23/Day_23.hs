e4257206990c6de4cd68993087578e770224c8b7
Raphaël <raphael.montes@epita.fr>
2024-12-23 18:38:54 +0100

    feat(2024/Day_23): added bonus part

--- 2024/Day_23/Day_23.hs
+++ 2024/Day_23/Day_23.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import System.Environment
+import Data.Algorithm.MaximalCliques
+import Data.Function
 import Data.List
 import Data.List.Split
 import Data.Maybe
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Algorithm.MaximalCliques
-import Data.Function
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 type Input = Map String [String]
 type Output = Int
@@ -37,10 +39,28 @@ partTwo input = intercalate "," . sort $ maxClique
           isAdjacent a = (`elem` (input Map.! a))
           maxClique = maximumBy (compare `on` length) $ getMaximalCliques isAdjacent nodes
 
+bronKerbosch :: Input -> [[String]]
+bronKerbosch graph = bronKerbosch' Set.empty nodes Set.empty
+    where nodes = Set.fromList $ Map.keys graph
+          bronKerbosch' :: Set String -> Set String -> Set String -> [[String]]
+          bronKerbosch' r p x | Set.null p && Set.null x = [Set.toList r]
+                              | otherwise = res
+            where (_, _, res) = Set.foldr go (x, p, []) p
+                  go v (x, p, res) = (Set.insert v x, Set.delete v p, res ++ bronKerbosch' r' p' x')
+                    where r' = Set.insert v r
+                          p' = Set.intersection p $ Set.fromList (graph Map.! v)
+                          x' = Set.intersection x $ Set.fromList (graph Map.! v)
+
+partBonus :: Input -> String
+partBonus = intercalate "," . sort .
+            maximumBy (compare `on` length) .
+            bronKerbosch
+
 compute :: Input -> String -> IO ()
 compute input "parse" = print input
 compute input "one"   = print . partOne $ input
 compute input "two"   = putStrLn . partTwo $ input
+compute input "bonus" = putStrLn . partBonus $ input
 compute input _       = error "Unknown part"
 
 main = do
