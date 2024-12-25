3128b337bc56acd9fdb5e3f28f951486f4e399a4
Raphaël <raphael.montes@epita.fr>
2024-12-25 12:55:56 +0100

    feat(2024/Day_25): Merry christmas!

---
+++ 2024/Day_25/Day_25.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import System.Environment
+import Data.List.Split
+import Data.List
+import Data.Tuple.Extra
+
+type Input = ([[Int]], [[Int]])
+type Output = Int
+
+parseInput :: String -> Input
+parseInput input = both (map asColCount) schematics
+    where schematics = partition (all (== '#') . head) . map lines . splitOn "\n\n" $ input
+          asColCount = map (length . head . group) . transpose
+    
+
+partOne :: Input -> Output
+partOne (keys, locks) = length [(key, lock) | key <- keys,
+                                              lock <- locks,
+                                              fits key lock]
+    where fits key = and . zipWith (<=) key
+
+partTwo :: Input -> Output
+partTwo = error "Not Implemented"
+
+compute :: Input -> String -> IO ()
+compute input "parse" = print input
+compute input "one"   = print . partOne $ input
+compute input "two"   = print . partTwo $ input
+compute input _       = error "Unknown part"
+
+main = do
+    args  <- getArgs
+    input <- parseInput <$> readFile (last args)
+    mapM (compute input) $  init args 
