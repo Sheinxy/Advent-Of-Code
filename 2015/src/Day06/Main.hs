f4dc4d4ac1a59bb65a5976adeb86d7b340a247aa
Sheinxy <sheinxy@seascow.win>
2024-12-28 15:15:56 +0100

    fix(2015/Day06): formatting

--- 2015/src/Day06/Main.hs
+++ 2015/src/Day06/Main.hs
@@ -1,14 +1,23 @@
 module Day06.Main (day06) where
 
 import           AOC           (submit)
+import           Control.Monad
 import           Data.Array.IO
 import           Data.Char     (isDigit)
 import           Data.Foldable (forM_)
 import           Data.List     (isInfixOf)
 
-data State = On | Off deriving (Show, Eq, Enum)
-data InstrType = Toggle | TurnOff | TurnOn deriving (Show, Eq, Enum)
-data Instruction = Instruction { instructionType :: InstrType, instructionRange :: [(Int, Int)] } deriving Show
+data State = On
+           | Off deriving (Show, Eq, Enum)
+
+data InstrType = Toggle
+               | TurnOff
+               | TurnOn deriving (Show, Eq, Enum)
+
+data Instruction = Instruction
+                 { instructionType  :: InstrType
+                 , instructionRange :: [(Int, Int)]
+                 } deriving Show
 
 toggleState :: State -> State
 toggleState On  = Off
@@ -20,9 +29,10 @@ type Output = Int
 parseInput :: String -> Input
 parseInput = map parseInstruction . lines
     where parseInstruction :: String -> Instruction
-          parseInstruction s | "on"     `isInfixOf` s = Instruction TurnOn  range
-                             | "off"    `isInfixOf` s = Instruction TurnOff range
-                             | otherwise              = Instruction Toggle  range
+          parseInstruction s
+            | "on"     `isInfixOf` s = Instruction TurnOn  range
+            | "off"    `isInfixOf` s = Instruction TurnOff range
+            | otherwise              = Instruction Toggle  range
             where s'        = dropWhile (not . isDigit) s
                   [a, _, b] = words s'
                   (i, j)    = read ("(" ++ a ++ ")") :: (Int, Int)
@@ -31,13 +41,16 @@ parseInput = map parseInstruction . lines
 
 partOne :: Input -> IO Output
 partOne input = do
-        arr <- newArray ((0, 0), (999, 999)) Off :: IO (IOArray (Int, Int) State)
+        arr <- newArray ((0, 0), (999, 999)) Off
+            :: IO (IOArray (Int, Int) State)
         forM_ input $ \ (Instruction t r) -> do
             forM_ r $ \ pos -> do
                 case t of
                     TurnOn  -> writeArray arr pos On
                     TurnOff -> writeArray arr pos Off
-                    _       -> readArray arr pos >>= writeArray arr pos . toggleState
+                    _       ->  readArray arr pos
+                            >>= writeArray arr pos
+                            .   toggleState
         elems <- getElems arr
         return (length . filter (== On) $ elems)
 
@@ -55,9 +68,9 @@ partTwo input = do
         return (sum elems)
 
 day06 :: String -> String -> IO ()
-day06 "parse" input = print . parseInput $ input
-day06 "one"   input = (partOne . parseInput) input >>= print
-day06 "two"   input = (partTwo . parseInput) input >>= print
-day06 "sone"  input = (partOne . parseInput) input >>= submit 2015 6 1 . show
-day06 "stwo"  input = (partTwo . parseInput) input >>= submit 2015 6 2 . show
-day06 _       input = error "Undefined part"
+day06 "parse" = print . parseInput
+day06 "one"   = print <=< partOne . parseInput
+day06 "two"   = print <=< partTwo . parseInput
+day06 "sone"  = submit 2015 6 1 . show <=< partOne . parseInput
+day06 "stwo"  = submit 2015 6 2 . show <=< partTwo . parseInput
+day06 _       = error "Undefined part"
