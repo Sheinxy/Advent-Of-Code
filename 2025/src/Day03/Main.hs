63b6dc1520ee0a12148d071dbfb21fffe686dde2
Sheinxy <sheinxy@seascow.win>
2025-12-04 20:43:19 +0100

    feat(2025/Day03): done

--- 2025/src/Day03/Main.hs
+++ 2025/src/Day03/Main.hs
@@ -1,18 +1,27 @@
 module Day03.Main (day03) where
 
-import           AOC (submit)
+import           AOC       (submit)
+import           Data.List (maximumBy)
+import           Data.Ord  (comparing)
 
-type Input = String
+type Input = [String]
 type Output = Int
 
 parseInput :: String -> Input
-parseInput = undefined
+parseInput = lines
+
+findJoltage :: Int -> String -> Int
+findJoltage numBat = read . step numBat
+    where step 0 _ = []
+          step n battery = first : next
+            where (first, idx) = maximumBy (comparing fst) . drop (n - 1) . reverse $ zip battery [1 .. ]
+                  next = step (n - 1) . drop idx $ battery
 
 partOne :: Input -> Output
-partOne = undefined
+partOne = sum . map (findJoltage 2)
 
 partTwo :: Input -> Output
-partTwo = undefined
+partTwo = sum . map (findJoltage 12)
 
 day03 :: String -> String -> IO ()
 day03 "parse" = print . parseInput
