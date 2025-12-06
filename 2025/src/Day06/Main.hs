9aa4cec3046022e2579617f5254ff4459d91c6a8
Sheinxy <sheinxy@seascow.win>
2025-12-06 10:04:43 +0100

    feat(2025/Day06): done

--- 2025/src/Day06/Main.hs
+++ 2025/src/Day06/Main.hs
@@ -1,23 +1,34 @@
 module Day06.Main (day06) where
 
-import           AOC (submit)
+import           AOC              (submit)
+import           Data.List        (transpose, uncons)
+import           Data.List.Split  (splitWhen)
+import           Data.Maybe       (fromJust)
+import           Data.Tuple.Extra ((***))
 
-type Input = String
+type Input = [(Int -> Int -> Int, [Int])]
 type Output = Int
 
-parseInput :: String -> Input
-parseInput = undefined
+parseOperator :: String -> (Int -> Int -> Int)
+parseOperator "*" = (*)
+parseOperator "+" = (+)
+parseOperator _   = error "Something went wrong, please check your input."
 
-partOne :: Input -> Output
-partOne = undefined
+partOne :: String -> Input
+partOne = map ((parseOperator *** map read) . fromJust . uncons . reverse)
+        . transpose . map words . lines
 
-partTwo :: Input -> Output
-partTwo = undefined
+partTwo :: String -> Input
+partTwo = map parseProblem . splitWhen (all (== ' ')) . transpose . lines
+    where parseProblem [] = error "Something went wrong, please check your input."
+          parseProblem (x : xs) = (parseOperator [last x], map read $ init x : xs)
+
+compute :: Input -> Output
+compute = sum . map (uncurry foldl1)
 
 day06 :: String -> String -> IO ()
-day06 "parse" = print . parseInput
-day06 "one"   = print . partOne . parseInput
-day06 "two"   = print . partTwo . parseInput
-day06 "sone"  = submit 2025 6 1 . show . partOne . parseInput
-day06 "stwo"  = submit 2025 6 2 . show . partTwo . parseInput
-day06 _       = error "Undefined part"
+day06 "one"  = print . compute . partOne
+day06 "two"  = print . compute . partTwo
+day06 "sone" = submit 2025 6 1 . show . compute . partOne
+day06 "stwo" = submit 2025 6 2 . show . compute . partTwo
+day06 _      = error "Undefined part"
