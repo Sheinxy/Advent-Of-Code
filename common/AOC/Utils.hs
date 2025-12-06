abc36a8219b8cf7b00499122128b11d03f64ec8a
Sheinxy <sheinxy@seascow.win>
2025-12-06 01:37:10 +0100

    feat(general): Utils.hs

---
+++ common/AOC/Utils.hs
@@ -0,0 +1,34 @@
+module AOC.Utils where
+
+-- Iterate until a fixed-point is found
+fixedPoint :: Eq a => (a -> a) -> a -> a
+fixedPoint f x = let x' = f x in
+    if x == x' then x
+    else fixedPoint f x'
+
+-- Is a between b and c included
+between :: Ord a => a -> a -> a -> Bool
+between a b c = b <= a && a <= c
+
+-- Is a between b and c excluded
+between' :: Ord a => a -> a -> a -> Bool
+between' a b c = b < a && a < c
+
+-- Is a in a closed range
+isInRange :: Ord a => a -> (a, a) -> Bool
+isInRange a (b, c)= between a b c
+
+-- Is a in an open range
+isInRange' :: Ord a => a -> (a, a) -> Bool
+isInRange' a (b, c) = between' a b c
+
+-- Similar to groupBy but without the transitive property
+-- Each element in the sublist matches the predicate with at
+-- least one other element in the list before it.
+groupByNT :: (a -> a -> Bool) -> [a] -> [[a]]
+groupByNT predicate = groupByNT' []
+    where groupByNT' acc [] = [reverse acc]
+          groupByNT' [] (x : xs) = groupByNT' [x] xs
+          groupByNT' acc l@(x : xs) | any (predicate x) acc = groupByNT' (x : acc) xs
+                                    | otherwise = (reverse acc) : groupByNT' [] l
+                                
