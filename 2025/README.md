822796a3b8ab288e464274b7bebbe58f5f472955
Sheinxy <sheinxy@seascow.win>
2025-12-10 19:28:56 +0100

    feat(2025/Day09): done

--- 2025/README.md
+++ 2025/README.md
@@ -13,7 +13,7 @@ Watch me code in Haskell for 12 days straight
 | 06 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day06/Main.hs) | [WriteUp](./writeups/Day06) |✅|✅|
 | 07 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day07/Main.hs) | [WriteUp](./writeups/Day07) |✅|✅|
 | 08 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day08/Main.hs) | [WriteUp](./writeups/Day08) |✅|✅|
-| 09 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day09/Main.hs) | [WriteUp](./writeups/Day09) |  |  |
+| 09 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day09/Main.hs) | [WriteUp](./writeups/Day09) |✅|✅|
 | 10 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day10/Main.hs) | [WriteUp](./writeups/Day10) |  |  |
 | 11 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day11/Main.hs) | [WriteUp](./writeups/Day11) |  |  |
 | 12 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day12/Main.hs) | [WriteUp](./writeups/Day12) |  |  |
--- 2025/src/Day09/Main.hs
+++ 2025/src/Day09/Main.hs
@@ -1,18 +1,58 @@
 module Day09.Main (day09) where
 
-import           AOC (submit)
+import           AOC       (submit)
+import           AOC.Utils (isInRange)
 
-type Input = String
+type Point = (Int, Int)
+type Segment = (Point, Point)
+type Polygon = [Segment]
+
+type Input = [Point]
 type Output = Int
 
 parseInput :: String -> Input
-parseInput = undefined
+parseInput = map (\x -> read $ "(" ++ x ++ ")") . lines
+
+area :: (Int, Int) -> (Int, Int) -> Int
+area (a, b) (c, d) = (abs (a - c) + 1) * (abs (b - d) + 1)
 
 partOne :: Input -> Output
-partOne = undefined
+partOne tiles = maximum [area x y | x <- tiles, y <- tiles, x < y]
+
+getLoop :: Input -> Polygon
+getLoop tiles = zip tiles $ tail (cycle tiles)
+
+getRectangleEdges :: (Point, Point) -> Polygon
+getRectangleEdges ((x1, y1), (x2, y2)) = [(a, b)
+                                         | a@(x , y ) <- corners
+                                         , b@(x', y') <- corners
+                                         , a < b, x == x' || y == y'
+                                         ]
+    where corners = [(minX, minY), (maxX, minY), (minX, maxY), (maxX, maxY)]
+          (minX, maxX) = (min x1 x2 + 1, max x1 x2 - 1)
+          (minY, maxY) = (min y1 y2 + 1, max y1 y2 - 1)
+
+edgeIntersects :: Segment -> Segment -> Bool
+edgeIntersects ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4))
+        | den == 0 = False
+        | otherwise =  isInRange px (min x1 x2, max x1 x2) && isInRange py (min y1 y2, max y1 y2)
+                    && isInRange px (min x3 x4, max x3 x4) && isInRange py (min y3 y4, max y3 y4)
+    where den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+          numPx = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)
+          numPy = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)
+          px = numPx `div` den
+          py = numPy `div` den
+
+intersectsPolygon :: Polygon -> Segment -> Bool
+intersectsPolygon poly segment = any (edgeIntersects segment) poly
 
 partTwo :: Input -> Output
-partTwo = undefined
+partTwo tiles = maximum
+              . map (uncurry area)
+              . filter (not . any (intersectsPolygon loop) . getRectangleEdges)
+              $ rects
+    where rects = [(x, y) | x <- tiles, y <- tiles, x < y]
+          loop  = getLoop tiles
 
 day09 :: String -> String -> IO ()
 day09 "parse" = print . parseInput
