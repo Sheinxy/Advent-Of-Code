876cbedb420eab7632235cc3cec0de514af786c4
Sheinxy <sheinxy@seascow.win>
2025-12-11 20:44:04 +0100

    feat(2025/Day11): done

--- 2025/src/Day11/Main.hs
+++ 2025/src/Day11/Main.hs
@@ -1,18 +1,33 @@
 module Day11.Main (day11) where
 
-import           AOC (submit)
+import           AOC                   (submit)
+import           Data.Function.Memoize (memoFix)
+import           Data.Map              ((!))
+import qualified Data.Map              as M
 
-type Input = String
+type Input = M.Map String [String]
 type Output = Int
 
 parseInput :: String -> Input
-parseInput = undefined
+parseInput = M.fromList . map parseLine . lines
+    where parseLine raw = let (key, ':' : rest) = break (== ':') raw in (key, words rest)
+
+countPathFromTo :: String -> String -> Input -> Output
+countPathFromTo from to input = memoFix go from
+    where go f key | key == to    = 1
+                   | key == "out" = 0
+                   | otherwise = sum [f next | next <- input ! key]
 
 partOne :: Input -> Output
-partOne = undefined
+partOne = countPathFromTo "you" "out"
 
 partTwo :: Input -> Output
-partTwo = undefined
+partTwo input | fftToDac /= 0 && dacToFft /= 0 = error "Something went wrong. Please check your input. (There can't be both a path from fft to dac and from dac to fft)"
+              | fftToDac == 0 = countPathFromTo "svr" "dac" input * dacToFft * countPathFromTo "fft" "out" input
+              | otherwise = countPathFromTo "svr" "fft" input * fftToDac * countPathFromTo "dac" "out" input
+    where fftToDac = countPathFromTo "fft" "dac" input
+          dacToFft = countPathFromTo "dac" "fft" input
+
 
 day11 :: String -> String -> IO ()
 day11 "parse" = print . parseInput
