98fb6f5b388e44db7207f7ac523ab832b7699d34
Sheinxy <sheinxy@seascow.win>
2025-12-12 20:39:45 +0100

    feat(2025/Day12): The end of AOC2025

--- 2025/README.md
+++ 2025/README.md
@@ -16,5 +16,5 @@ Watch me code in Haskell for 12 days straight
 | 09 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day09/Main.hs) | [WriteUp](./writeups/Day09) |✅|✅|
 | 10 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day10/Main.hs) | [WriteUp](./writeups/Day10) |✅|✅|
 | 11 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day11/Main.hs) | [WriteUp](./writeups/Day11) |✅|✅|
-| 12 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day12/Main.hs) | [WriteUp](./writeups/Day12) |  |  |
+| 12 | [Code](https://github.com/Sheinxy/Advent-Of-Code/blob/main/2025/src/Day12/Main.hs) | [WriteUp](./writeups/Day12) |✅|✅|
 
--- 2025/src/Day12/Main.hs
+++ 2025/src/Day12/Main.hs
@@ -1,18 +1,37 @@
 module Day12.Main (day12) where
 
-import           AOC (submit)
+import           AOC             (submit)
+import           Data.List.Split (splitOn)
+import           GHC.Utils.Misc  (count)
 
-type Input = String
+type Present = [String]
+data Region = Region { regionSize :: (Int, Int), regionPresents :: [Int] } deriving Show
+data Input  = Input { presents :: [Present], regions :: [Region] } deriving Show
+
 type Output = Int
 
 parseInput :: String -> Input
-parseInput = undefined
+parseInput raw = Input { presents = shapes', regions = regions' }
+    where input = splitOn [""] . lines $ raw
+          (shapes, regions) = (init input, last input)
+          shapes' = map tail shapes
+          regions' = map parseRegion regions
+          parseRegion r = let (regSize, ':' : regPres) = span ( /= ':') r
+                          in Region (parseSize regSize) (map read . words $ regPres)
+          parseSize s = let (a, 'x' : b) = span ( /= 'x') s
+                        in (read a, read b)
+
+isTriviallyValid :: Region -> Bool
+isTriviallyValid reg = numBlocks <= x' * y'
+    where (x, y) = regionSize reg
+          (x', y') = (x `div` 3, y `div` 3)
+          numBlocks = sum . regionPresents $ reg
 
 partOne :: Input -> Output
-partOne = undefined
+partOne = count isTriviallyValid . regions
 
-partTwo :: Input -> Output
-partTwo = undefined
+partTwo :: Input -> String
+partTwo = const "Merry Christmas! <3"
 
 day12 :: String -> String -> IO ()
 day12 "parse" = print . parseInput
